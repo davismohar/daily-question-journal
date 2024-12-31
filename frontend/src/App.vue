c82a8511aecd5caf2425eba1e07d7fe5b5472d4f
Davis Mohr <davis.mohar@missionlane.com>
2024-12-31 15:28:27 -0500

    add new logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,14 +9,6 @@ const isAuthenticated = computed(() => auth0.isAuthenticated);
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    class="logo"
-    src="./assets/logo.svg"
-    width="125"
-    height="125"
-  />
-
   <div class="wrapper">
     <nav>
       <RouterLink to="/">Home</RouterLink>
